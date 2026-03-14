d434ac5e266efc2fce3efcd48749df4c49d90e24
Rexirium <rexirium@qq.com>
2026-03-14 16:15:34 +0800

    two bit gate CZ, CX implemented and AbstractBasis assigned

--- exactdiagonalize.jl
+++ exactdiagonalize.jl
@@ -4,34 +4,57 @@ using SparseArrays
 include("utils.jl")
 
 abstract type AbstractOpSum end
+abstract type AbstractBasis end
 
 mutable struct SpinOpSum <: AbstractOpSum
     type::DataType
     opvec::AbstractVector
 end
 
+mutable struct NumBasis <: AbstractBasis
+    num::Int
+    bitsvec::Vector{<:Int}
+
+    NumBasis(lsize::Int, num::Int) = new(num, numbitbasis(lsize, num))
+end
+
 function act(opstr::String, loc::Int, bits::Int, T::DataType)
+    """
+    act a single qubit operator on the state `bits`=|1001011⟩ for bits=(1001011)₂
+    |1⟩ = (1, 0)ᵀ = |↑⟩, |0⟩ = (0, 1)ᵀ = |↓⟩
+    I do not specify the Y operator (has complex element) to keep type stability.
+    """
     if opstr == "Z"
         return bits, T(2 * readbit(bits, loc) - 1)
     elseif opstr == "X"
         return flip(bits, loc), one(T)
+    elseif opstr == "Sp" # means simplectic matrix [0 1 ; -1 0], Sp = iY
+        return flip(bits, loc), T(1 - 2 * readbit(bits, loc))
     elseif opstr == "σ+"
         return flip(bits, loc), T(! readbit(bits, loc))
     elseif opstr == "σ-"
         return flip(bits, loc), T(readbit(bits, loc))
-    # We do not specify the Y operator to keep type stability
     else
         error("Operator not specified yet!")
     end
 end
 
 function act(opstr::String, loc::Tuple{Int, Int}, bits::Int, T::DataType)
-    if opstr == "CZ"
-
+    if opstr == "CX"
+        c, t = loc
+        bitc = readbit(bits, c)
+        return flip(bits, t, bitc), one(T)
+    elseif opstr == "CZ"
+        c, t = loc
+        i1, i2 = minmax(c, t)
+        b1, b2 = readbit(bits, i1, i2)
+        return bits, T(2 * (b1 ^ b2) - 1)
+    else
+        error("Operator not specified yet!")
     end
 end
 
-function trans(op::Tuple, bits::Int, T::DataType)
+function apply(op::Tuple, bits::Int, T::DataType)
     element = op[1]
     newbits = bits
     oplen = length(op)
@@ -44,23 +67,35 @@ function trans(op::Tuple, bits::Int, T::DataType)
     return newbits, element
 end
 
-function apply!(hmat::Matrix{T}, ops::SpinOpSum, basis::Vector{<:Int}, idx::Int, num::Int) where T <: Number
-    bits = basis[idx]
-    for op in ops.opvec
-        newbits, element = trans(op, bits, T)
-        if count_ones(newbits) == num && ! iszero(element)
-            newidx = findfirst(==(newbits), basis)
-            hmat[newidx, idx] += element
+function makeHamiltonian(ops::AbstractOpSum, basis::NumBasis)
+    dim = length(basis.bitsvec)
+    hmat = zeros(ops.type, dim, dim)
+    for (j, bits) in enumerate(basis.bitsvec)
+        for op in ops.opvec
+            newbits, element = apply(op, bits, ops.type)
+            if count_ones(newbits) == basis.num && !iszero(element)
+                i = searchsortedfirst(basis.bitsvec, newbits)
+                hmat[i, j] += element
+            end
         end
     end
+    return Hermitian(hmat)
 end
 
+let 
+    L, N = 10, 5
 
-function makeHamiltonian(ops::AbstractOpSum, basis::Vector{<:Int}, num::Int)
-    dim = length(basis)
-    hmat = zeros(ops.type, dim, dim)
-    for j in 1:dim
-        apply!(hmat, ops, basis, j, num)
+    os = Tuple[]
+    for j in 1:L
+        nj = mod1(j+1, L)
+        push!(os, (1.0, "Z", j, "Z", nj))
+        push!(os, (1.0, "X", j, "X", nj))
+        push!(os, (-1.0, "Sp", j, "Sp", nj))
     end
-    return Hermitian(hmat)
+    # push!(os, (1.0, "X", L))
+    ops = SpinOpSum(Float64, os)
+
+    basis = NumBasis(L, N)
+    @time Hnum = makeHamiltonian(ops, basis)
+    nothing
 end
--- utils.jl
+++ utils.jl
@@ -1,16 +1,28 @@
 function readbit(bits::Int, pos::Int)::Bool
-    return (bits >> (pos - 1)) & 1 == 1
+    return (bits >> (pos - 1)) & true == true
+end
+
+function readbit(bits::Int, i1::Int, i2::Int)::Tuple{Bool, Bool}
+    shifted = bits >> (i1 - 1)
+    b1 = shifted & true == true
+    shifted >>= (i2 - i1)
+    b2 = shifted & true == true
+    return b1, b2
 end
 # count number of ones between bit i and j (excluding i and j), i<j 
 # and return the sign according to the odd or even of the number 
 function signbetween(bits::Int, i::Int, j::Int)
-    mask = 1<<(j-i-1) -1
+    mask = 1<<(j-i-1) - 1
     segbits = (bits>>i) & mask
     return (-1)^count_ones(segbits)
 end
 
 function flip(bits::Int, pos::Int)::Int
-    return bits ⊻ (1 << (pos -1))
+    return bits ⊻ (1 << (pos - 1))
+end
+
+function flip(bits::Int, pos::Int, b::Bool)::Int
+    return bits ⊻ (b << (pos - 1))
 end
 
 function splitbasis(bits::Int, b::Int)
@@ -22,9 +34,9 @@ end
 
 function numbitbasis(len::Int, num::Int)
     """
-    generating all the L-bit integers with N bits read 1.
+    generating all the len-bit integers with num bits read 1.
     """
-    num > len && error("N is larger than L")
+    num > len && error("more ones than total bits")
     num == 0 && return Int[0]
     basis = Int[]
     sizehint!(basis, binomial(len, num))
